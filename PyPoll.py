84dac87783005f3a47863e273028d5f666d709ed
Mots94 <rmotykow@gmail.com>
2022-03-24 17:09:03 -0500

    Working Code

--- PyPoll.py
+++ PyPoll.py
@@ -1,31 +1,93 @@
-#Data that needs to be retrived
-#1. Total number of votes cast
-#2. Complete list of candidates who received votes
-#3. Percentage of votes each candidate won
-#4. Total number of votes each candidate won
-#5. Winner of election based on votes received
-
-# #Creating path to .csv file
-# file_to_load = "Resources\election_results.csv"
-
-# #Create variable to open file path variable
-# with open(file_to_load) as election_results:
-
-#     #Analysis
-#     print(election_results)
-
-# import csv
-# import os
-# file_to_load = os.path.join("Resources", "election_results.csv")
-
-# with open(file_to_load) as election_results:
-#     print(election_results)
 import csv
 import os
 
+
+#Variable assigned to load file from path
 file_to_load = os.path.join("Resources", "election_results.csv")
 
+#Variable assigned to save file to path
 file_to_save = os.path.join("Analysis", "election_analysis.txt")
 
-#Use open statement to write data to text file
+# Initialize vote count variable
+total_votes = 0
+
+# Create list of candidates
+candidate_options1 = []
+
+# Creat dictionary for candidate names and # of votes
+candidate_votes = {}
+
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+
+# Open election_results.csv and read file
 with open(file_to_load) as election_data:
+
+    #Read and analyze data
+    file_reader = csv.reader(election_data)
+
+    #Read the header row
+    headers = next(file_reader)
+
+    #Print each row in CSV file
+    for row in file_reader:
+
+        #Adding up each row to count total votes
+        total_votes += 1
+
+        #Create candidate name variable to add to candidate_options list
+        candidate_name = row[2]
+        
+        # Add candidate_name data to canidate_options list, use if statment 
+        # so the name of each candidate appears only once in candidate_options list
+        if candidate_name not in candidate_options1: 
+            
+            # Add candidate name to candidate options list
+            candidate_options1.append(candidate_name)
+        
+    
+           #Tracking candidate vote count
+            candidate_votes[candidate_name] = 0
+
+        # # Add a vote to the candidate's name 
+        candidate_votes[candidate_name] += 1
+  
+
+# with open(file_to_save), "w") as txt.file:
+
+    # election_results = (
+    #     f"\nElection Results\n"
+    #     f"-------------------------\n"
+    #     f"Total Votes: {total_votes:,}\n"
+    #     f"-------------------------\n")
+    # print(election_results, end="")
+
+    # text_file.write(election_results)
+
+    for candidate_name in candidate_votes:
+
+        votes = candidate_votes[candidate_name]
+
+        vote_percentage = (float(votes)/float(total_votes)) * 100
+
+        # Print out each candidate's name, vote count, and percentage of votes
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+
+            winning_count = votes
+
+            winning_percentage = vote_percentage
+
+            winning_candidate = candidate_name
+
+        winning_candidate_summary = (
+            f"------------------------\n"
+            f"Winner: {winning_candidate}\n"
+            f"Winning Vote Count: {winning_count:,}\n"
+            f"Winning Percentage: {winning_percentage:.1f}\n"
+            f"------------------------\n")
+
+    print(winning_candidate_summary)
